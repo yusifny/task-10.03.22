36745950305a7f1344a1ad1020be4911d5533e1e
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-10 09:55:54 +0400

    file created
    file created

---
+++ ArrayElementsModulus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 285, -44, 131, -117, -58, 265, 97, -105};
+            int i = 0;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    arr[i] = -arr[i];
+            }
+
+            Console.Write("Array Elements(modulus): ");
+            for (i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
