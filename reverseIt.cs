236f2221911d61914934deb966b2f8207986c15d
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-10 09:34:06 +0400

    file created
    file created

---
+++ reverseIt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mehods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "BabeK";
+            string reverse = "";
+            int length = str.Length - 1;
+
+            while (length >= 0)
+            {
+                reverse += str[length];
+                length--;
+            }
+
+            Console.WriteLine(reverse);
+        }
+    }
+}
